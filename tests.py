9299db633579ff00a5575e3617b301ab68c470bb
Sveta Ulyankina <ts380106078@yandex.ru>
2024-03-25 22:18:43 +0300

    Add several test functions

--- tests.py
+++ tests.py
@@ -1,24 +1,49 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_add_new_book_add_book_and_this_book_is_on_list(self):
         collector = BooksCollector()
+        name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(name)
+        assert len(collector.books_genre) == 1 and list(collector.books_genre.keys())[0] == name
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_long_name_book_is_not_added(self):
+        collector = BooksCollector()
+        name = 'Удивительное путешествие Нильса Хольгерссона с дикими гусями по Швеции'
+        collector.add_new_book(name)
+        assert len(collector.books_genre) == 0
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_set_book_genre_add_genre_for_book(self):
+        collector = BooksCollector()
+        name = '12 стульев'
+        collector.add_new_book(name)
+        collector.set_book_genre(name,'Комедии')
+        assert list(collector.books_genre.values())[0] == 'Комедии'
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre_adding_undefined_genre_is_not_added(self):
+        collector = BooksCollector()
+        name = 'Как относиться к себе и людям'
+        collector.add_new_book(name)
+        collector.set_book_genre(name,'Психология')
+        assert list(collector.books_genre.values())[0] == ''
+
+    def test_get_book_genre_book_genre_found(self):
+        collector = BooksCollector()
+        name = 'Мастер и Маргарита'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, 'Фантастика')
+        assert collector.get_book_genre(name) == 'Фантастика'
+
+    def test_get_books_with_specific_genre_get_fantastic_genre(self):
+        collector = BooksCollector()
+        name_1 = 'Портрет Дориана Грея'
+        name_2 = 'Грозовой перевал'
+        collector.add_new_book(name_1)
+        collector.add_new_book(name_2)
+        collector.set_book_genre(name_1, 'Фантастика')
+        collector.set_book_genre(name_2, 'Детективы')
+        assert len(collector.get_books_with_specific_genre('Фантастика')) == 1
+
+    def test_get_books_genre_
