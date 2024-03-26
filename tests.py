4c1e8e412a7a41b77949afd891168b33ad854470
Sveta Ulyankina <ts380106078@yandex.ru>
2024-03-26 21:58:06 +0300

    Updated README.md

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pytest
 from main import BooksCollector
 
+
 class TestBooksCollector:
 
     def test_add_new_book_add_book_and_this_book_is_on_list(self):
@@ -29,12 +30,19 @@ class TestBooksCollector:
         collector.set_book_genre(name,'Психология')
         assert list(collector.books_genre.values())[0] == ''
 
-    def test_get_book_genre_book_genre_found(self):
+    @pytest.mark.parametrize(
+        'name,genre',
+        [
+            ['Мастер и Маргарита', 'Фантастика'],
+            ['Отцы и дети', 'Комедии'],
+            ['Песнь Льда и Пламени', 'Ужасы']
+        ]
+    )
+    def test_get_book_genre_book_genre_found(self, name, genre):
         collector = BooksCollector()
-        name = 'Мастер и Маргарита'
         collector.add_new_book(name)
-        collector.set_book_genre(name, 'Фантастика')
-        assert collector.get_book_genre(name) == 'Фантастика'
+        collector.set_book_genre(name, genre)
+        assert collector.get_book_genre(name) == genre
 
     def test_get_books_with_specific_genre_get_fantastic_genre(self):
         collector = BooksCollector()
@@ -56,15 +64,20 @@ class TestBooksCollector:
         collector.set_book_genre(name_2, 'Детективы')
         assert len(collector.get_books_genre()) == 2
 
-    def test_get_books_for_children_books_with_age_genre_not_included_in_list(self):
+    @pytest.mark.parametrize(
+        'name_age_1,genre_age_1,name_age_2,genre_age_2',
+        [
+            ['Маленький принц', 'Мультфильмы', 'Заводной апельсин', 'Ужасы'],
+            ['Звёздные войны', 'Фантастика', 'Агата Кристи', 'Детективы']
+        ]
+    )
+    def test_get_books_for_children_books_with_age_genre_not_included_in_list(self, name_age_1, genre_age_1, name_age_2, genre_age_2):
         collector = BooksCollector()
-        name_1 = 'Маленький принц'
-        name_2 = 'Заводной апельсин'
-        collector.add_new_book(name_1)
-        collector.add_new_book(name_2)
-        collector.set_book_genre(name_1, 'Мультфильмы')
-        collector.set_book_genre(name_2, 'Ужасы')
-        assert collector.get_books_for_children()[0] == name_1
+        collector.add_new_book(name_age_1)
+        collector.add_new_book(name_age_2)
+        collector.set_book_genre(name_age_1, genre_age_1)
+        collector.set_book_genre(name_age_2, genre_age_2)
+        assert collector.get_books_for_children()[0] == name_age_1 and len(collector.get_books_for_children()) == 1
 
     def test_add_book_in_favorites_book_added_in_favorites(self):
         collector = BooksCollector()
@@ -72,7 +85,7 @@ class TestBooksCollector:
         collector.add_new_book(name)
         collector.set_book_genre(name, 'Мультфильмы')
         collector.add_book_in_favorites(name)
-        assert collector.favorites == [name]
+        assert collector.favorites[0] == name
 
     def test_delete_book_from_favorites_book_deleted_from_favorites(self):
         collector = BooksCollector()
@@ -83,7 +96,7 @@ class TestBooksCollector:
         collector.delete_book_from_favorites(name)
         assert len(collector.favorites) == 0
 
-    def test_get_list_of_favorites_books_favorites_book_in_list(self):
+    def test_get_list_of_favorites_books_favorites_two_books_in_list(self):
         collector = BooksCollector()
         name_1 = 'Облачный атлас'
         name_2 = 'Марсианин'
